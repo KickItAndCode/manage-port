cce2ce7cc32e188545dc4bb9c4997e3668c2a1ec
Rob Henderson <kickitandcode@gmail.com>
2025-05-30 16:00:41 -0700

    starter project for manage port

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <h1 className="text-4xl font-bold mb-4">Hello World</h1>
+      <p className="text-lg">Welcome to Manage Port!</p>
+    </div>
+  );
+}
